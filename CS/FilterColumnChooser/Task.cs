ecd3d771b496f30a6f60d41253f52372615d222a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/FilterColumnChooser/Task.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace FilterColumnChooserExample
+{
+    public class Task
+    {
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime FinishDate { get; set; }
+        public bool IsComplete { get; set; }
+
+        public object COl1 { get { return "test #1"; } }
+        public object COl2 { get { return "test #2"; } }
+        public object COl3 { get { return "test #3"; } }
+        public object COl4 { get { return "test #4"; } }
+        public object COl5 { get { return "test #5"; } }
+        public object COl6 { get { return "test #6"; } }
+        public object COl7 { get { return "test #7"; } }
+
+        public static ObservableCollection<Task> GetList {
+            get {
+                ObservableCollection<Task> collection = new ObservableCollection<Task>();
+                for (int i = 0; i < 5; i++) {
+                    Random rnd = new Random(i);
+                    DateTime startDate = DateTime.Now.AddDays(i);
+                    collection.Add(new Task {
+                        Name = "Task " + i,
+                        StartDate = startDate,
+                        FinishDate = startDate.AddDays(rnd.Next(10, 20)),
+                        IsComplete = rnd.Next(2) == 0
+                    });
+                }
+                return collection;
+            }
+        }
+    }
+}
---
+++ VB/FilterColumnChooser/MainWindow.xaml.vb
@@ -0,0 +1,31 @@
+﻿Imports DevExpress.Xpf.Grid
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Windows
+
+Namespace FilterColumnChooserExample
+	Partial Public Class MainWindow
+		Inherits Window
+
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			Dim band As New GridControlBand() With {.Header = "Band" & gcMain.Bands.Count}
+			If gcMain.Bands.Count = 0 Then
+				Dim columns As List(Of GridColumn) = gcMain.Columns.ToList()
+				gcMain.Columns.Clear()
+				For Each item In columns
+					band.Columns.Add(item)
+				Next item
+			End If
+			gcMain.Bands.Add(band)
+		End Sub
+
+		Private Sub btn_ShowChooser_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			view.ShowColumnChooser()
+		End Sub
+	End Class
+End Namespace
